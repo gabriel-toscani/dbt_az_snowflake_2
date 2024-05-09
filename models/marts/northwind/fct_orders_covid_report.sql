d6550ae59e863d319c1f9e622cd46b772983d9ea
Gabriel Toscani <gabrieltoscani96@gmail.com>
2024-05-09 02:05:54 +0000

    Finishing fct_covid_report

--- models/marts/northwind/fct_orders_covid_report.sql
+++ models/marts/northwind/fct_orders_covid_report.sql
@@ -4,7 +4,7 @@ with
         from {{ref('int_order_and_detail')}}
     )
 
-    , us_cities as (
+    , us_states as (
         select *
         from {{ref('seed_us_cities_states_counties')}}
     )
@@ -14,4 +14,22 @@ with
         from {{ref('int_covid_report')}}
     )
 
-    
+    , revenue_by_state as (
+        select
+            us_states."State full" as state
+            , sum(total_revenue) as total_revenue
+        from orders_and_details
+        left join us_states on orders_and_details.shipcity = us_states."City"
+        group by us_states."State full"
+    )
+
+    , add_covid_data as (
+        select 
+            covid_report.*
+            , total_revenue
+        from covid_report
+        left join revenue_by_state on covid_report.state = revenue_by_state.state
+    )
+
+select *
+from add_covid_data
